469f08355b969f19b31a6f85265f97223a3693cb
lucky <singh22.lakhi@gmail.com>
2026-05-31 18:34:00 +0530

    add the cors origin * access from all

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: 'http://localhost:5173', // cors means Backend allows requests FROM frontend running on port 5173 ex:request: POST.GET,PUT etc
+        origin: '*', // cors means Backend allows requests FROM frontend running on port 5173 ex:request: POST.GET,PUT etc
         credentials: true
     })
 )

90a03025ee16486553b3a123bbc609245c437b83
lucky <singh22.lakhi@gmail.com>
2026-05-31 18:57:08 +0530

    add server host and port in the vite.config.js file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 5173,
+  },
   plugins: [
     react(),
     tailwindcss(),

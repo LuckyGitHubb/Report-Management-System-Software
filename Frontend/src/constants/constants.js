3b251ebb6bc8075fbf14277fe7552555fdb1eb17
lucky <singh22.lakhi@gmail.com>
2026-06-15 01:27:30 +0530

    add backend url

--- Frontend/src/constants/constants.js
+++ Frontend/src/constants/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://report-management-system-software-4-joq4.onrender.com";
+export const API_BASE_URL = "https://report-management-system-software-5.onrender.com";
 // export const API_BASE_URL = "http://localhost:4000";    
